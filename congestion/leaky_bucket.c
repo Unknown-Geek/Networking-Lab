4fb33d36e1c8fd9785cb80c2f030e883cbf971f8
Shravan Nander Pandala <97742905+Unknown-Geek@users.noreply.github.com>
2026-03-13 00:59:50 +0530

    Refactor leaky bucket implementation for clarity

--- congestion/leaky_bucket.c
+++ congestion/leaky_bucket.c
@@ -22,7 +22,7 @@
 
 int main() {
         srand(time(0));
-        int packets[PACKET_COUNT], i, rate, bucket_size, time_to_transmit, clk, output;
+        int packets[PACKET_COUNT], i, rate, bucket_size;
         int remaining_size = 0;
 
         for (int i = 0; i < PACKET_COUNT; i++)
@@ -34,37 +34,24 @@ int main() {
         scanf("%d", &bucket_size);
         printf("\n");
 
-        i = 0;
-        while (i < PACKET_COUNT || remaining_size > 0) {
+        for (int i = 0; i < PACKET_COUNT || remaining_size > 0; i++) {
                 if (i < PACKET_COUNT) {
-                        if ((packets[i] + remaining_size) > bucket_size)
+                        if (packets[i] + remaining_size > bucket_size)
                                 printf("Bucket overflow! Packet %d dropped\n", packets[i]);
                         else {
                                 remaining_size += packets[i];
-                                printf("Incoming packet size: %d\n", packets[i]);
-                                printf("Bytes remaining to transmit: %d\n\n", remaining_size);
-                        }
-                        i++;
-                }
-
-                time_to_transmit = (rand() % 4 + 1) * 10;
-                printf("Time left for transmission: %d units\n\n", time_to_transmit);
-
-                for (clk = 10; clk <= time_to_transmit; clk += 10) {
-                        sleep(1);
-                        if (remaining_size) {
-                                if (remaining_size <= rate) {
-                                        output = remaining_size;
-                                        remaining_size = 0;
-                                } else {
-                                        output = rate;
-                                        remaining_size -= rate;
-                                }
-                                printf("Packet %d transmitted\n", output);
-                                printf("Bytes remaining to transmit: %d\n\n", remaining_size);
-                        } else {
-                                printf("No packets to transmit!\n\n");
+                                printf("Incoming packet size: %d, bucket: %d\n", packets[i], remaining_size);
                         }
                 }
+
+                sleep(1);
+                int sent;
+                if (remaining_size < rate)
+                        sent = remaining_size;
+                else
+                        sent = rate;
+                        
+                remaining_size -= sent;
+                printf("Transmitted: %d, bucket: %d\n\n", sent, remaining_size);
         }
 }
