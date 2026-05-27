b28a8e00c206a6ecf0eb15043e2063cc4dfec016
Unknown-Geek <shravanpandala@gmail.com>
2026-05-27 20:25:23 +0530

    Update TCP multi-client and multi-server files

--- TCP/multi_client.c
+++ TCP/multi_client.c
@@ -12,74 +12,72 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
 #include <unistd.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
-#include <time.h>
 #include <pthread.h>
 
-void *send_data(void *arg){
-    int client_sock = *((int *)arg);
+char *ip = "127.0.0.1";
+int client_sock;
+socklen_t server_size;
+struct sockaddr_in server_addr;
+int port = 4533;
+
+void *send_data(void *arg) {
     char buffer[1024];
 
-    bzero(buffer,1024);
     while(1) {
+        bzero(buffer,1024);
         gets(buffer);
-        buffer[strlen(buffer)] = '\0'; //Attach null character of each message to be sent
 
-        if(strcmp(buffer,"SEND") == 0) {
-            send(client_sock,buffer,strlen(buffer)+1,0);
+        if(strcmp(buffer,"SEND") == 0){    
+            buffer[strlen(buffer)] = '\0';
+            send(client_sock,buffer,sizeof(buffer),0);
 
             bzero(buffer,1024);
             printf("Enter data to send : ");
             gets(buffer);
             buffer[strlen(buffer)] = '\0';
-            send(client_sock,buffer,strlen(buffer)+1,0);
+            send(client_sock,buffer,sizeof(buffer),0);
         }
     }
 }
 
-void *receive_data(void *arg){
-    int client_sock = *((int *)arg);
+void *receive_data(void *arg) {
     char buffer[1024];
-    int len;
+    int n=0;
 
-    bzero(buffer,1024);
     while(1) {
-        len = recv(client_sock,buffer,sizeof(buffer),0);
-        if (len>0) 
-            printf("%s\n",buffer);
         bzero(buffer,1024);
+        n = recv(client_sock,buffer,sizeof(buffer),0);
+        if(n > 0) {
+            printf("%s\n",buffer);
+        }
     }
 }
 
 int main() {
-    char *ip = "127.0.0.1";
-    socklen_t server_size;
-    struct sockaddr_in server_addr;
-    int client_sock;
-    int port = 4533;
     char buffer[1024];
     pthread_t send_thread,receive_thread;
 
+    server_size = sizeof(server_addr);
+
     client_sock = socket(AF_INET,SOCK_STREAM,0);
-    if (client_sock < 0) {
-        printf("Socket error.\n");
+    if(client_sock < 0) {
+        printf("Socket Error.\n");
         exit(1);
     }
 
-    server_size = sizeof(server_addr);
-
-    memset(&server_addr,'\0',server_size);
+    memset(&server_addr,'\0', server_size);
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
     server_addr.sin_addr.s_addr = inet_addr(ip);
 
-    connect(client_sock,(struct sockaddr *)&server_addr, server_size);
-
-    pthread_create(&send_thread,NULL,send_data,&client_sock);
-    pthread_create(&receive_thread,NULL,receive_data,&client_sock);
+    connect(client_sock,(struct sockaddr *) &server_addr, server_size);
+    
+    pthread_create(&send_thread,NULL,send_data,NULL);
+    pthread_create(&receive_thread,NULL,receive_data,NULL);
 
     pthread_join(send_thread,NULL);
     pthread_join(receive_thread,NULL);
--- TCP/multi_server.c
+++ TCP/multi_server.c
@@ -14,67 +14,62 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
 #include <unistd.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <string.h>
-#include <time.h>
 #include <pthread.h>
+#include <string.h>
 
 #define SIZE 5
 
-typedef struct{
-    int client_sock;
-    struct sockaddr_in client_addr;
-    socklen_t client_size;
+typedef struct {
     int index;
+    int client_sock;
+    socklen_t client_size;
+    struct sockaddr_in client_addr;
 } Client;
+
+char *ip = "127.0.0.1";
+int server_sock;
+socklen_t server_size;
+struct sockaddr_in server_addr;
+int port = 4533;
+int count = 0;
 
 Client clients[SIZE];
 pthread_t client_thread[SIZE];
-int count = 0;
 
 void *client_communication(void *arg) {
-    Client *c = (Client *) arg;
-    int index = c->index;
-    socklen_t client_size = c->client_size;
-    struct sockaddr_in client_addr = c->client_addr;
-    int client_sock = c->client_sock;
-
     char buffer[1024];
     char message[1024];
-    bzero(buffer,1024);
-    bzero(message,1024);
+    int index = clients[count].index;
+    int client_sock = clients[count].client_sock;
+    socklen_t client_size = clients[count].client_size;
+    struct sockaddr_in client_addr = clients[count].client_addr;
 
     while(1) {
-        int len = recv(client_sock,buffer,sizeof(buffer),0);
-        buffer[len] = '\0';
+        bzero(buffer,1024);
+        recv(client_sock,buffer,sizeof(buffer),0);
+
         if(strcmp(buffer,"SEND") == 0) {
             bzero(buffer,1024);
-            bzero(message,1024);
+            recv(client_sock,buffer,sizeof(buffer),0);
 
-            len = recv(client_sock,buffer,sizeof(buffer),0);
-            buffer[len] = '\0';
+            bzero(message,1024);
+            sprintf(message,"Client %d : %s",index,buffer);
+            printf("%s\n",message);
+
             for(int i=0 ; i<count ; i++) {
-                if (i == index)
+                if (index == i)
                     continue;
-                else {
-                    sprintf(message,"Client %d : %s",index,buffer);
-                    printf("%s\n",message);
-                    message[strlen(message)] = '\0';
-                    send(clients[i].client_sock,message,strlen(message),0);
-                } 
+                else 
+                    send(clients[i].client_sock,message,sizeof(message),0);
             }
         }
     }
 }
 
 int main() {
-    char *ip = "127.0.0.1";
-    socklen_t server_size;
-    struct sockaddr_in server_addr;
-    int client_sock,server_sock;
-    int port = 4533;
     char buffer[1024];
     int n;
 
@@ -86,28 +81,28 @@ int main() {
 
     server_size = sizeof(server_addr);
 
-    memset(&server_addr,'\0',server_size);
+    memset(&server_addr,'\0', server_size);
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
     server_addr.sin_addr.s_addr = inet_addr(ip);
 
-    n = bind(server_sock,(struct sockaddr *)&server_addr, server_size);
+    n = bind(server_sock,(struct sockaddr *) &server_addr, server_size);
     if(n < 0) {
         printf("Bind Error.\n");
         exit(1);
     }
 
-    printf("Server running on port %d\n",port);
+    printf("Server running on port %d.\n",port);
+
     listen(server_sock,SIZE);
 
     while(1) {
         clients[count].client_size = sizeof(clients[count].client_addr);
-        clients[count].client_sock = accept(server_sock,(struct sockaddr *)&clients[count].client_addr, &clients[count].client_size);
+        clients[count].client_sock = accept(server_sock,(struct sockaddr *)& clients[count].client_addr, &clients[count].client_size);
         clients[count].index = count;
-        pthread_create(&client_thread[count],NULL,client_communication,&clients[count]);
+        pthread_create(&client_thread[count],NULL,client_communication,NULL);
         printf("Client %d connected.\n",count);
         count++;
     }
-
     close(server_sock);
 }
